4e04e9225e1e28f382ef0e51ac11055e76788669
Cameron W. D. Elvidge <cameron@iversoft.ca>
2020-08-20 07:31:53 -0400

    add focus

--- src/Layout.js
+++ src/Layout.js
@@ -7,20 +7,19 @@ import {
   forceManyBody,
   forceCollide,
 } from "d3-force";
+import { easeQuadInOut, easeCircleIn, easeQuadIn } from "d3";
 
 const mapWithKey = _.map.convert({ cap: false });
 
-const radius = (node) => node.size;
+const radius = (node) => node.scale * 200;
 
-const initial = {
-  nodes: [
-    { size: 200, text: "guards! guards!" },
-    { size: 200, text: "A Poison seeping into the bones" },
-    { size: 200, text: "old one awakes"},
-    { size: 200 },
-    { size: 200 },
-  ],
-};
+const initial = [
+  { text: "guards! guards!" },
+  { text: "A Poison seeping into the bones" },
+  { text: "old one awakes" },
+  {},
+  {},
+];
 
 const forceFocus = (focus) => {
   var nodes;
@@ -32,10 +31,16 @@ const forceFocus = (focus) => {
       node = nodes[i];
 
       const isFocus = i === focus;
-      const target = isFocus ? 300 : 200;
-      const diff = (target - node.size) / 100.0;
+      const target = isFocus ? 1.2 : 1;
 
-      node.size += diff * 2;
+      if (node.scale === target) {
+        continue;
+      }
+
+      const diff = target - node.prevScale;
+      const t = 1 - alpha;
+
+      node.scale = node.prevScale + diff * easeQuadIn(t);
     }
   };
 
@@ -46,11 +51,15 @@ const forceFocus = (focus) => {
   return force;
 };
 
+const initializeNodes = (nodes) => {
+  return _.map((node) => ({ ...node, scale: 1 }), nodes);
+};
+
 const useSimulation = () => {
   const simulation = useRef();
   const collide = useRef();
 
-  const [state, setState] = useState(initial);
+  const [state, setState] = useState({ nodes: [] });
   const [focus, setFocus] = useState(null);
 
   const update = () => {
@@ -59,13 +68,13 @@ const useSimulation = () => {
     }
 
     simulation.current.force("collide").radius(radius);
-    setState({ nodes: simulation.current.nodes() })
+    setState({ nodes: simulation.current.nodes() });
   };
 
   useLayoutEffect(() => {
     collide.current = forceCollide().strength(0.1).radius(radius).iterations(1);
 
-    simulation.current = forceSimulation(initial.nodes)
+    simulation.current = forceSimulation(initializeNodes(initial))
       .force("collide", collide.current)
       .force("body", forceManyBody().strength(50))
       .force("center", forceCenter())
@@ -79,30 +88,31 @@ const useSimulation = () => {
       return;
     }
 
-    simulation.current
-      .force("focus", forceFocus(focus))
-      .alpha(1)
-      .restart();
-  }, [focus]);
+    simulation.current.nodes().forEach((node) => {
+      node.prevScale = node.scale;
+    });
 
+    simulation.current.force("focus", forceFocus(focus)).alpha(1).restart();
+  }, [focus]);
 
   return { state, setFocus };
 };
 
 const Circle = (props) => {
-  const { x, y, size, debug = false, onClick, text } = props;
+  const { x, y, scale, debug = false, onClick, text } = props;
 
   const id = useRef(_.uniqueId());
+
   const arcp = 200 * 0.75;
-
-  const scale = size / 200;
-
   const className = classnames({ debug });
 
   return (
     <g className={className} transform={`translate(${x},${y}) scale(${scale})`}>
       <defs>
-        <path id={id.current} d={`M ${-arcp} 0 A ${arcp} ${arcp} 0 0 1 ${arcp} 0`} />
+        <path
+          id={id.current}
+          d={`M ${-arcp} 0 A ${arcp} ${arcp} 0 0 1 ${arcp} 0`}
+        />
       </defs>
       <circle onClick={onClick} className="bounds" r={200} />
       <circle r={200 * 0.7} />
@@ -116,13 +126,12 @@ const Circle = (props) => {
 };
 
 const Layout = (props) => {
-  const { state, restart, setFocus } = useSimulation();
+  const { state, setFocus } = useSimulation();
 
   return (
     <svg
       viewBox="-1080 -1920 2160 3840"
       preserveAspectRatio="xMidYMid meet"
-      onClick={restart}
     >
       {mapWithKey(
         (node, index) => (
@@ -130,10 +139,10 @@ const Layout = (props) => {
             onClick={() => setFocus(index)}
             key={index}
             text={node.text}
-            size={node.size}
+            scale={node.scale}
             x={node.x}
             y={node.y}
-            debug={false}
+            debug={true}
           />
         ),
         state.nodes
