d7269f6770f2bab00e6248ee4d39e31dbf2be514
Cameron W. D. Elvidge <cameron@iversoft.ca>
2020-08-21 16:23:32 -0400

    add version widget

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import Layout from "./Layout";
 
 import "./App.css";
+import Version from "./Version";
 
 function App() {
   return (
-    <Layout />
+    <>
+      <Layout />
+      <Version />
+    </>
   );
 }
 
---
+++ src/Version.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./version.module.css";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return {
+    data,
+    error,
+    loading: data === null && error === null,
+  };
+};
+
+const splitAt = (index) => (x) => [x.slice(0, index), x.slice(index)];
+
+const Version = () => {
+  const { data } = useFetch(`${process.env.PUBLIC_URL}/version.json`);
+
+  if (!data) {
+    return null;
+  }
+
+  const { hash } = data;
+  const [start, rest] = splitAt(7)(hash);
+
+  const url = `https://github.com/cwedavies/clocks/tree/${hash}`;
+
+  return (
+    <a className={styles.root} href={url}>
+      <span className={styles.start}>{start}</span>
+      <span className={styles.rest}>{rest}</span>
+    </a>
+  );
+};
+
+export default Version;
