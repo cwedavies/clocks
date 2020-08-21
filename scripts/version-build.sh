13b989d1e6f40374f1c9799b21b8e2385002609f
Cameron W. D. Elvidge <cameron@iversoft.ca>
2020-08-21 15:37:44 -0400

    add version to deploymeny

---
+++ scripts/version-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cat << EOF
+{
+  "commit": "${GITHUB_SHA:-$(git rev-parse HEAD)}",
+  "buildDate": "$(date  -u +%FT%TZ)",
+  "ref": "${GITHUB_REF:-$(git rev-parse --abbrev-ref HEAD)}"
+  "runId": "${GITHUB_RUN_ID:-}",
+  "runNumber": "${GITHUB_RUN_NUMBER:-}"
+}
+EOF
