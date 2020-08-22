3a0c1f68b6c09aaa2490d1815714647503032f64
Cameron W. D. Elvidge <cameron@iversoft.ca>
2020-08-22 11:35:54 -0400

    fix version script

--- scripts/version-build.sh
+++ scripts/version-build.sh
@@ -8,7 +8,7 @@ cat << EOF
 {
   "commit": "${GITHUB_SHA:-$(git rev-parse HEAD)}",
   "buildDate": "$(date  -u +%FT%TZ)",
-  "ref": "${GITHUB_REF:-$(git rev-parse --abbrev-ref HEAD)}"
+  "ref": "${GITHUB_REF:-$(git rev-parse --abbrev-ref HEAD)}",
   "runId": "${GITHUB_RUN_ID:-}",
   "runNumber": "${GITHUB_RUN_NUMBER:-}"
 }
