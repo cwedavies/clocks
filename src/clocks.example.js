1eb84f6bd633b24c9280f67b2902f304623c3115
Cameron W. D. Elvidge <cameron@iversoft.ca>
2020-08-22 22:05:56 -0400

    add new clock designs

--- src/clocks.example.js
+++ src/clocks.example.js
@@ -1,6 +1,6 @@
 export default [
   { text: "The house is on fire", marks: 4 },
-  { text: "guards! guards!", marks: 3 },
+  { text: "guards! guards!", marks: 3, variant: "fancy" },
   "A Poison seeping into the bones",
   "old one awakes",
   { text: "the Marquis onside", variant: "small" },
--- src/components/clocks/index.js
+++ src/components/clocks/index.js
@@ -2,13 +2,15 @@ import _ from "lodash/fp";
 import Basic from "./Basic";
 import Cluster from "./Cluster";
 import Small from "./Small";
+import Fancy from "./Fancy";
 
 const registry = {
   basic: Basic,
   cluster: Cluster,
+  fancy: Fancy,
   small: Small,
 };
 
-export const getClockComponent = (variant) => _.getOr(Basic, variant, registry);
+export const getClockComponent = (variant) => _.getOr(Fancy, variant, registry);
 
 export default getClockComponent;
